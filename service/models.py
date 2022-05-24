7edc0da5592fd4229dcb00d46c3f2a041d985adb
future <twixxx2457@gmail.com>
2022-05-24 17:08:22 +0300

    pr 21 36^50

--- service/models.py
+++ service/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+
+    title = models.CharField(verbose_name='Название', max_length=100)
+    description = models.TextField(verbose_name='Описание', null=True, blank=True)
+    image = models.ImageField(verbose_name='Картинка')
+    created_at = models.DateTimeField(verbose_name='Дата создания', auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name='Дата изменения', auto_now=True)
