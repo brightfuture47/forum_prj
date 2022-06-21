265af2e0231a09e576fec904da85f80f0cfcbb76
future <twixxx2457@gmail.com>
2022-06-21 19:30:18 +0300

    pr 22 - 24:36

--- service/urls.py
+++ service/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from service import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.PostsView.as_view(), name='index'),
+    path('post/<int:pk>', views.DetailPostsView.as_view(), name='detail_post'),
     path('about', views.about, name='about'),
 ]
--- service/views.py
+++ service/views.py
@@ -1,9 +1,18 @@
 from urllib.parse import urlparse
 from django.shortcuts import render, redirect
 from service.models import Post, Comment
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 def index(req):
     return render(req, 'index.html')
 
 def about(req):
     return render(req, 'about.html')
+
+class PostsView(ListView):
+    model = Post
+    template_name = "index.html"
+
+class DetailPostsView(DetailView):
+    model = Post
+    template_name = "detail_post.html"
