281c78662f04bd4686628773df4ba1bbbcf1bd1e
future <twixxx2457@gmail.com>
2022-07-13 18:03:39 +0300

    PR 26 final

--- service/views.py
+++ service/views.py
@@ -5,7 +5,9 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 from .forms import PostForm, CommentForm, UserRegisterForm
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
-
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
 
 
 def index(req):
@@ -15,9 +17,9 @@ def about(req):
     return render(req, 'about.html')
 
 
-
-class RegisterForm(CreateView):
+class RegisterForm(SuccessMessageMixin, CreateView):
     form_class = UserRegisterForm
+    success_message = "%(username)s was created successfully" 
     template_name = 'register.html'
     success_url = reverse_lazy('login')
 
@@ -30,11 +32,29 @@ class DetailPostView(DetailView):
     model = Post
     template_name = "detail_post.html"
 
-class CreatePostView(PermissionRequiredMixin, CreateView):
-    permission_required = 'service.add_post'
-    model = Post
-    template_name = "create_post.html"
-    form_class = PostForm 
+#class CreatePostView(PermissionRequiredMixin, CreateView):
+#    permission_required = 'service.add_post'
+#    model = Post
+#    template_name = "create_post.html"
+#    form_class = PostForm 
+
+@login_required
+@permission_required("service.add_post")
+def create_post(req):
+    form = PostForm()
+    if req.method == "POST":
+        form = PostForm(req.POST)
+        if form.is_valid():
+            form.save()
+            title = form.cleaned_data.get("title")
+            if title != "POST":
+                messages.error(req, f"Something went wrong")
+                return redirect('index')
+            #id = form.cleaned_data.get("pk")
+            messages.success(req, f"Post {title} was creates successfully")
+            return redirect('index')
+
+    return render(req, "create_post.html", {"form": form})
   
 class UpdatePostView(PermissionRequiredMixin, UpdateView):
     permission_required = 'service.change_post'
