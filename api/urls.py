4f16b49186f4affe9034d913c71e9333899802d1
future <twixxx2457@gmail.com>
2022-07-25 23:16:03 +0300

    pr 30 final

--- api/urls.py
+++ api/urls.py
@@ -12,6 +12,9 @@ urlpatterns = [
     #path('', include(router.urls)),
     path('checkbox_list', views.checkbox_list, name='checkbox_list'),
     path('checkbox_list/<int:pk>', views.checkbox_detail, name='checkbox_detail'),
+    path('checkbox_create', views.checkbox_create, name='checkbox_create'),
+    path('checkbox_update/<int:pk>', views.checkbox_update, name='checkbox_update'),
+    path('checkbox_delete/<int:pk>', views.checkbox_delete, name='checkbox_delete'),
 ]
 
 
--- api/views.py
+++ api/views.py
@@ -28,4 +28,30 @@ def checkbox_detail(req, pk):
         return Response (( f"error: Checkbox with id = {pk} is not found"), status = status.HTTP_404_NOT_FOUND)
     return Response(serializer.data)
 
-    
+
+@api_view(['POST'])
+def checkbox_create(req):
+    serializer = CheckboxSerializer(data=req.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['PUT'])
+def checkbox_update(req, pk):
+    try:
+        checkbox = Checkbox.objects.get(id=pk)
+        serializer = CheckboxSerializer(checkbox, data=req.data)
+        if serializer.is_valid():
+            serializer.save()
+    except Checkbox.DoesNotExist:
+        return Response (( f"error: Checkbox with id = {pk} is not found"), status = status.HTTP_404_NOT_FOUND)
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def checkbox_delete(req, pk):
+    checkbox = Checkbox.objects.get(id=pk)
+    checkbox.delete()
+    return Response(status = status.HTTP_204_NO_CONTENT)
+
