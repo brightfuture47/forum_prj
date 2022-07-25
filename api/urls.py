412b914c755abb8dc58cd6e04ae8440669f1ff72
future <twixxx2457@gmail.com>
2022-07-25 22:23:24 +0300

    pr 30 23^10

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,18 @@
 from django.db import router
 from django.urls import path, include
 from rest_framework import routers
-from api.views import CheckboxViewSet
 
-router = routers.DefaultRouter()
-router.register('checkbox', CheckboxViewSet)
+from api import views
+#from api.views import CheckboxViewSet
+
+#router = routers.DefaultRouter()
+#router.register('checkbox', CheckboxViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    #path('', include(router.urls)),
+    path('checkbox_list', views.checkbox_list, name='checkbox_list'),
+    path('checkbox_list/<int:pk>', views.checkbox_detail, name='checkbox_detail'),
 ]
+
+
+
--- api/views.py
+++ api/views.py
@@ -2,9 +2,30 @@ from django.forms import CharField
 #from django.shortcuts import render
 
 from rest_framework import viewsets
+from api import serializers
 from api.models import Checkbox
 from api.serializers import CheckboxSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
-class CheckboxViewSet(viewsets.ModelViewSet):
-    queryset = Checkbox.objects.all()
-    serializer_class = CheckboxSerializer
+#class CheckboxViewSet(viewsets.ModelViewSet):
+#    queryset = Checkbox.objects.all()
+#    serializer_class = CheckboxSerializer
+
+@api_view(['GET'])
+def checkbox_list(req):
+    checkboxes = Checkbox.objects.all()
+    serializer = CheckboxSerializer(checkboxes, many=True)
+    return Response(serializer.data)
+    
+@api_view(['GET'])
+def checkbox_detail(req, pk):
+    try:
+        checkbox = Checkbox.objects.get(id=pk)
+        serializer = CheckboxSerializer(checkbox)
+    except Checkbox.DoesNotExist:
+        return Response (( f"error: Checkbox with id = {pk} is not found"), status = status.HTTP_404_NOT_FOUND)
+    return Response(serializer.data)
+
+    
